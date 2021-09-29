84c5e702342a3d5c269ed0b384eea0470ec2a8ab
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2021-09-29 17:41:09 -0400

    Minor Changes to Level2 - Added Game Over

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Respawn : MonoBehaviour
 {
+    [SerializeField] private Text gameOver;
+
+    void Start()
+    {
+        gameOver.enabled = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Score.scoreValue -= 10;
-            if (Score.scoreValue < 0)
+            if (Score.scoreValue < 1)
+            {
                 Score.scoreValue = 0;
+                gameOver.enabled = true;
+
+            }
         }
     }
 }
